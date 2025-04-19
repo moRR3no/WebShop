11e932020ebaf5a5ae8c812992f98c0d81d416c8
Konrado <konradpstrongowski@gmail.com>
2025-04-19 20:11:17 +0200

    Generic Repository implementation

---
+++ Web_Shop.Persistence/Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace Web_Shop.Persistence.Repositories.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        IGenericRepository<T> WithTracking();
+        IGenericRepository<T> WithoutTracking();
+
+        IQueryable<T> Entities { get; }
+
+        Task<T?> GetByIdAsync(params object?[]? id);
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity, params object?[]? id);
+        Task DeleteAsync(T entity);
+        Task<bool> Exists(params object?[]? id);
+    }
+}
